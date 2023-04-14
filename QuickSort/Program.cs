8dea81968d1104d4c093421edbb0eaa1bd953ec5
LavenderPixl <annajacobsen1999@hotmail.com>
2023-04-14 14:01:02 +0200

    Nearly done! just need to fix the sorting of custom array.

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//QuickSort is a sorting algorithm, used for sorting Arrays, in a specified order - In this case, Lowest Value to Highest.
+
+int[] arr = { 1, 22, 61, 20, 45, 30, 43 };
+
+int low = 0;
+int high = 6;
+
+int pivot = arr[6]; // Which number we're using to compare TO, in the array. Is on the LAST space in the array.
+int loop = 0; // Which loop we're currently on + Which space we're comparing WITH, in the array.
+
+if (arr[loop] <= pivot)
+{
+
+}
