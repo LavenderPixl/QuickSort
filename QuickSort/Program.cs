650f1863dd9fd53e5508cda8f613fa9b86171ed9
LavenderPixl <annajacobsen1999@hotmail.com>
2023-04-17 08:48:37 +0200

    QuickSort fixed.

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,14 +1,104 @@
-﻿//QuickSort is a sorting algorithm, used for sorting Arrays, in a specified order - In this case, Lowest Value to Highest.
+﻿using System.Reflection.Emit;
 
-int[] arr = { 1, 22, 61, 20, 45, 30, 43 };
+Console.WriteLine("=== Welcome to SortRUs ===\n");
 
-int low = 0;
-int high = 6;
+int[] arr = { 10, 23, 2, 446, 8, 45 };
+int arrLength = arr.Length;
 
-int pivot = arr[6]; // Which number we're using to compare TO, in the array. Is on the LAST space in the array.
-int loop = 0; // Which loop we're currently on + Which space we're comparing WITH, in the array.
-
-if (arr[loop] <= pivot)
+Console.WriteLine("Press 1 to see a predetermined Array.\nPress 2 to create a custom array!");
+reTry:
+if (Int32.TryParse(Console.ReadLine(), out int l))
 {
+    switch (l)
+    {
+        case 1:
+            Console.Clear();
+            QuickSorting(arr, 0, arrLength - 1);
+            WriteArray(arr, arrLength);
+            break;
 
+        case 2:
+            Console.Clear();
+            int[] customArray = UserArray();
+            QuickSorting(customArray, 0, customArray.Length - 1);
+            WriteArray(customArray, customArray.Length);
+            break;
+
+        default:
+            Console.WriteLine("Try again.");
+            goto reTry;
+    }
 }
+else
+{
+    Console.WriteLine("Try again.");
+    goto reTry;
+}
+
+
+static int[] UserArray()
+{
+    Console.WriteLine("How many numbers are you working with?");
+    if (Int32.TryParse(Console.ReadLine(), out int arrLength))
+    {
+        int[] cArray = new int[arrLength];
+
+        Console.WriteLine("Please insert your numbers: ");
+        for (int pos = 0; pos < cArray.Length; pos++)
+        {
+            if (Int32.TryParse(Console.ReadLine(), out int arrNumber))
+            {
+                cArray[pos] = arrNumber;
+            }
+            else
+            {
+                pos--;
+            }
+        }
+        return cArray;
+    }
+    return new int[0];
+}
+static void WriteArray(int[] arr, int size)
+{
+    Console.WriteLine("Array in ascending order: ");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i] + " | ");
+    }
+}
+
+static int Part(int[] arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            Swap(arr, i, j);
+        }
+    }
+    Swap(arr, i + 1, high);
+    return (i + 1);
+}
+static void QuickSorting(int[] arr, int low, int high)
+{
+    if (low < high)
+    {
+        int partitionPos = Part(arr, low, high);
+
+        QuickSorting(arr, low, partitionPos - 1);
+        QuickSorting(arr, partitionPos + 1, high);
+    }
+}
+static void Swap(int[] arr, int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+
